74daab3008ec973d1ee8e89abee7c43d2bd2e9b6
sswellington <wellingtons@id.uff.br>
2020-07-08 22:30:40 -0300

    feat: functions
    * open_json
    	Open a file json of the disk and the function return this file json
    	Use the lib `json` - RFC 7159
    		https://docs.python.org/3/library/json.html?highlight=json#module-json
    	...
    	Parameters:
    	file : str
    		path where is the json file
    	Return : datatypes
    		json file
    
    * submit
    	Send messagem for address
    	Use the lib `request`
    		https://requests.readthedocs.io/en/master/
    	...
    	Parameters:
    	addressee : str
    		url of addressee
    	message : datatypes
    		message content in json format
    	Return : unsigned int
    		get HTTP Status Messages
    		more information:
    		https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status

--- scicrop-api-test-python.py
+++ scicrop-api-test-python.py
@@ -1,2 +1,49 @@
+import json
+import requests as req
+
+def open_json(file):
+	"""
+	Open a file json of the disk and the function return this file json
+	Use the lib `json` - RFC 7159
+		https://docs.python.org/3/library/json.html?highlight=json#module-json	
+	...
+	Parameters:
+	file : str
+		path where is the json file
+	Return : datatypes
+		json file
+	"""
+	with open(file) as f:
+		return (json.load(f))
+
+def submit(addressee, message): 
+	"""
+	Send messagem for address
+	Use the lib `request`
+		https://requests.readthedocs.io/en/master/
+	...
+	Parameters:
+	addressee : str
+		url of addressee
+	message : datatypes
+		message content in json format  
+	Return : unsigned int
+		get HTTP Status Messages 
+		more information: 
+		https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+	"""
+	ack = req.post(url=addressee, json=message)
+	return (ack.status_code)
+
 if __name__ == '__main__':
-	pass
+	data = open_json('test.json')
+	url = 'https://engine.scicrop.com/scicrop-engine-web/api/v1/jobs/post_resume'
+	
+	try:
+		ack = submit(url,data)
+		if (ack == 200) :
+			print("Message sent successfully")
+		else :
+			print("Error sending message:",ack)
+	except req.exceptions.RequestException as e:
+		raise SystemExit(e)
